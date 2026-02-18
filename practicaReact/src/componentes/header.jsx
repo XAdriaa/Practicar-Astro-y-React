71a9335edc903c783e733e470755ea6699ac9bd4
XAdriaa <s.adria.navarro@gmail.com>
2026-02-18 12:41:04 +0100

    Inicio del proyecto react

---
+++ practicaReact/src/componentes/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const header = () => {
+  return (
+	<div className='container'>
+		<div className='row'>
+			<div className='col-12'>
+				<img src='/public/logo_provisional.png'></img>
+			</div>
+		</div>
+	</div>
+  )
+}
+
+export default header
---
+++ practicaReact/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const index = () => {
+  return (
+	<div>index</div>
+  )
+}
+
+export default index
