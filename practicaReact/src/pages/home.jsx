a538b58c02c4f841d251d9e892c215593046220c
XAdriaa <s.adria.navarro@gmail.com>
2026-02-19 09:05:54 +0100

    Home iniciada primera banda echa, voy por la segunda

--- practicaReact/src/pages/home.jsx
+++ practicaReact/src/pages/home.jsx
@@ -4,8 +4,36 @@ const Home = () => {
   return (
 	<div className='container'>
 		<div className='row'>
-			<div className='col-12 d-flex justify-content-center align-items-center'>
-				<h1>Bienvenido a GAM</h1>
+			<div className='col-12 d-flex justify-content-center text-center'>
+				<div className='card'>
+					<img src='/public/san-bernardo.jpg' className='img-fluid card-img'/>
+				<div className='card-img-overlay text-light'>
+					<h1 className=''>No compres una zaça,regala una vida</h1>
+					<h5 className=''>Mes de 150 gosos esperant la teua ajuda</h5>
+					<button className='btn btn-light mt-2'>Adopta</button>
+				</div>
+				</div>
+			</div>
+		</div>
+		<div className='row'>
+			<div className='col-12'>
+				<div className='d-flex justify-content-center flex-column'>
+					<h3>Gossos recentment adoptats</h3>
+					<div className='d-flex flex-row justify-content-between'>
+						<div className='card w-50'>
+							<img src='/public/boyero.jpg' className='img-fluid card-img-top'/>
+							<div>
+								<p></p>
+							</div>
+						</div>
+						<div className='card w-50'>
+							<img src='/public/Pastor-aleman.png' className='img-fluid card-img-top'/>
+						</div>
+						<div className='card w-50'>
+							<img src='/public/golden-retriever.webp' className='img-fluid card-img-top'/>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
--- practicaReact/src/pages/layout.jsx
+++ practicaReact/src/pages/layout.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 const layout = () => {
   return (
-		<nav className="navbar navbar-expand-lg navbar-light">
+		<nav className="navbar navbar-expand navbar-light">
 			<ul className="navbar-nav mx-auto">
-				<li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-				<li className='nav-item'><Link className='nav-link' to="/adopta">Adopta</Link></li>
-				<li className='nav-item'><Link className='nav-link' to="/adiestra">Adiestra</Link></li>
-				<li className="nav-item"><Link className="nav-link" to="/contacte">Contacte</Link></li>
+				<li className="nav-item"><Link className="nav-link" to="/"><p>Home</p></Link></li>
+				<li className='nav-item'><Link className='nav-link' to="/adopta"><p>Adopta</p></Link></li>
+				<li className='nav-item'><Link className='nav-link' to="/adiestra"><p>Adiestra</p></Link></li>
+				<li className="nav-item"><Link className="nav-link" to="/contacte"><p>Contacte</p></Link></li>
 			</ul>
 		</nav>
   )
