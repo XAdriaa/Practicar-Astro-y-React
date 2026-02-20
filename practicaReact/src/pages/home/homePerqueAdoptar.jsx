56f223a3f6d1650382de6437f620f6f26909ffeb
XAdriaa <s.adria.navarro@gmail.com>
2026-02-20 14:35:44 +0100

    imagenes nuevas

--- practicaReact/src/pages/home/homePerqueAdoptar.jsx
+++ practicaReact/src/pages/home/homePerqueAdoptar.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 const HomePerqueAdoptar = () => {
   return (
-	<div>
-		
+	<div className='d-flex'>
+		<div className='card mx-3'>
+			<img src='/agradablesFoto.webp' className='card-img-top'/>
+			<div className='card-body'>
+				<p className='fs-5'>Son molt agraits</p>
+			</div>
+
+		</div>
+		<div className='card mx-3'>
+			<img src='/perros-niños.webp' className='card-img-top'/>
+			<div className='card-body'>
+				<p className='fs-5'>Ajuden als chiquets amb el desenvolupament</p>
+			</div>
+
+		</div>
+		<div className='card mx-3'>
+			<img src='/perrosMestizo.jpg' className='card-img-top'/>
+			<div className='card-body'>
+				<p className='fs-5'>Donar una nova oportunitat</p>
+			</div>
+
+		</div>
+		<div className='card mx-3'>
+			<img src='/perroMental.webp' className='card-img-top'/>
+			<div className='card-body'>
+				<p className='fs-5' >Ajuda a la teua salut mental</p>
+			</div>
+
+		</div>
 	</div>
   )
 }
