833c4adf12029618f43ce454bffb48299518da53
XAdriaa <s.adria.navarro@gmail.com>
2026-02-20 12:13:47 +0100

    testeo banner en movil

--- practicaReact/src/pages/home/home.jsx
+++ practicaReact/src/pages/home/home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
 				<div className='card'>
 					<img src='/san-bernardo.jpg' className='img-fluid card-img'/>
 				<div className='card-img-overlay text-light'>
-					<h1 className=''>No compres una zaça,regala una vida</h1>
-					<h5 className=''>Mes de 150 gosos esperant la teua ajuda</h5>
-					<button className='btn btn-light mt-2'>Adopta</button>
+					<h1 className='texto-banner-titulo'>No compres una zaça,regala una vida</h1>
+					<h5 className='texto-banner-subtitulo'>Mes de 150 gosos esperant la teua ajuda</h5>
+					<button className='btn btn-outline-light mt-2 boton-adopcion-banner'>Adopta</button>
 				</div>
 				</div>
 			</div>
