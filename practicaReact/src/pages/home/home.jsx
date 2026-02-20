cf4d08a2efbdbc45090e136abd40546c76928f47
XAdriaa <s.adria.navarro@gmail.com>
2026-02-20 13:31:25 +0100

    Titulo corregido

--- practicaReact/src/pages/home/home.jsx
+++ practicaReact/src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import HomeAdopciones from './homeAdopciones'
+import HomePerqueAdoptar from './homePerqueAdoptar'
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
 				<div className='card'>
 					<img src='/san-bernardo.jpg' className='img-fluid card-img'/>
 				<div className='card-img-overlay text-light'>
-					<h1 className='texto-banner-titulo'>No compres una zaça,regala una vida</h1>
+					<h1 className='texto-banner-titulo'>No compres una raça,regala una vida</h1>
 					<h5 className='texto-banner-subtitulo'>Mes de 150 gosos esperant la teua ajuda</h5>
 					<button className='btn btn-outline-light mt-2 boton-adopcion-banner'>Adopta</button>
 				</div>
@@ -24,6 +25,12 @@ const Home = () => {
 						<HomeAdopciones />
 					</div>
 				</div>
+			</div>
+		</div>
+		<div className='row'>
+			<div className='col-12 text-center'>
+				<h1>¿Perque adoptar?</h1>
+				<HomePerqueAdoptar />
 			</div>
 		</div>
 	</div>
---
+++ practicaReact/src/pages/home/homePerqueAdoptar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePerqueAdoptar = () => {
+  return (
+	<div>
+		
+	</div>
+  )
+}
+
+export default HomePerqueAdoptar
