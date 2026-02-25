84721aa0e0ae9f12fee1019b1e79e09f4e50af90
XAdriaa <s.adria.navarro@gmail.com>
2026-02-25 13:21:08 +0100

    test 2

--- practicaReact/src/componentes/footer.jsx
+++ practicaReact/src/componentes/footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
 	<footer className=' bordered border-top mt-5 bg-light'>
 		<div className='container'>
 			<div className='row'>
-				<div className='col-12 d-flex flex-sm-column justify-content-between align-items-center'>
+				<div className='col-12 d-flex flex-column flex-md-row justify-content-between align-items-center'>
 					<img src='/public/logo_provisional-sinfondo.png'/>
-					<div className='d-flex gap-4 flex-sm-column justify-content-center align-items-center'>
+					<div className='d-flex gap-4 flex-column flex-md-row justify-content-center align-items-center'>
 						<div>
-							<ul className='list-unstyled text-end text-sm-center'>
+							<ul className='list-unstyled text-md-end text-center'>
 								<li><p>Horari de visita de 09:00 a 19:00</p></li>
 								<li><p>Donacions: IBAN ES85 4502 6595 4152 4899</p></li>
 								<li><p>Avis legal</p></li>
@@ -20,7 +20,7 @@ const Footer = () => {
 							</ul>
 						</div>
 						<div>
-							<ul className='list-unstyled fs-2 d-flex flex-column flex-sm-row gap-sm-3'>
+							<ul className='list-unstyled fs-2 d-flex flex-md-column flex-sm-row gap-3'>
 								<li><a href='https://wa.me/34653452030?text=Hola!%20Quiero%20información'></a><Whatsapp className="bi bi-whatsapp" /></li>
 								<li><a href=''></a><Instagram className="bi bi-instagram" /></li>
 								<li><a href=''></a><TwitterX className="bi bi-twitter-x" /></li>
