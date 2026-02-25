db372a5b6bff45245b9be5480c8e102e39189289
XAdriaa <s.adria.navarro@gmail.com>
2026-02-25 12:39:22 +0100

    Com adoptar en movil esta preparat probar en produccio

--- practicaReact/src/pages/home/comAdoptra.jsx
+++ practicaReact/src/pages/home/comAdoptra.jsx
@@ -5,40 +5,40 @@ const ComAdoptra = () => {
   return (
     <div className='d-flex justify-content-center flex-wrap'>
         {/* Paso 1 */}
-        <div className="col-md-2 col-sm-4 mb-3 mx-3">
-          <div className="p-3 border rounded bg-light h-100">
+        <div className="col-md-2 col-sm-4 col-5 mb-3 mx-3">
+          <div className="p-3 border rounded bg-light h-100 carta-adoptar">
             <House size={40} className="text-primary mb-2" />
             <p className="small m-0 text-wrap">Visita la protectora</p>
           </div>
         </div>
 
         {/* Paso 2 */}
-        <div className="col-md-2 col-sm-4 mb-3 mx-3">
-          <div className="p-3 border rounded bg-light h-100">
+        <div className="col-md-2 col-sm-4 col-5 mb-3 mx-3">
+          <div className="p-3 border rounded bg-light h-100 carta-adoptar">
             <ArrowRepeat size={40} className="text-primary mb-2" />
             <p className="small m-0 text-wrap">Conec al gos</p>
           </div>
         </div>
 
         {/* Paso 3 */}
-        <div className="col-md-2 col-sm-4 mb-3 mx-3">
-          <div className="p-3 border rounded bg-light h-100">
+        <div className="col-md-2 col-sm-4 col-5 mb-3 mx-3">
+          <div className="p-3 border rounded bg-light h-100 carta-adoptar">
             <Clipboard size={40} className="text-primary mb-2" />
             <p className="small m-0 text-wrap">Formulari</p>
           </div>
         </div>
 
         {/* Paso 4 */}
-        <div className="col-md-2 col-sm-4 mb-3 mx-3">
-          <div className="p-3 border rounded bg-light h-100">
+        <div className="col-md-2 col-sm-4 col-5 mb-3 mx-3">
+          <div className="p-3 border rounded bg-light h-100 carta-adoptar">
             <ClipboardCheck size={40} className="text-primary mb-2" />
             <p className="small m-0 text-wrap">Pre-adopció</p>
           </div>
         </div>
 
         {/* Paso 5 */}
-        <div className="col-md-2 col-sm-4 mb-3 mx-3">
-          <div className="p-3 border rounded bg-success text-white h-100">
+        <div className="col-md-2 col-sm-4 col-5 mb-3 mx-3">
+          <div className="p-3 border rounded bg-success text-white h-100 carta-adoptar">
             <PatchCheck size={40} className="mb-2" />
             <p className="small m-0 text-wrap">¡Adoptat!</p>
           </div>
