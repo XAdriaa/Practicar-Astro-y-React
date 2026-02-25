ef4b56e98f26dc84ab903af16131b0caa55b24cc
XAdriaa <s.adria.navarro@gmail.com>
2026-02-25 15:06:11 +0100

    Ananlitic exo, text

--- practicaReact/src/app.jsx
+++ practicaReact/src/app.jsx
@@ -1,5 +1,5 @@
-
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react' // 1. Importar el componente
 import Header from './componentes/header'
 import Footer from './componentes/footer'
 import Home from './pages/home/home'
@@ -10,18 +10,21 @@ import NotFound from './componentes/404'
 
 const App = () => {
   return (
-	<div>
-    <Routes>
-      <Route path='/' element={<Header />}>
-        <Route index element={<Home />}></Route>
-        <Route path='contacte' element={<Contacte />}></Route>
-        <Route path='adopta' element={<Adopta />}></Route>
-        <Route path='adiestra' element={<Adiestra />}></Route>
-      </Route>
-      <Route path='*' element={<NotFound />}></Route>
-    </Routes>
-    <Footer />
-  </div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Header />}>
+          <Route index element={<Home />}></Route>
+          <Route path='contacte' element={<Contacte />}></Route>
+          <Route path='adopta' element={<Adopta />}></Route>
+          <Route path='adiestra' element={<Adiestra />}></Route>
+        </Route>
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
+      <Footer />
+
+      {/* 2. Añadir el componente aquí */}
+      <Analytics />
+    </div>
   )
 }
 
