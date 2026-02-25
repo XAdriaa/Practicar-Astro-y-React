fc752353148dfc7479786e4b277bc1cd04fc86ff
XAdriaa <s.adria.navarro@gmail.com>
2026-02-25 13:34:27 +0100

    enlaces bien puesto

--- practicaReact/src/componentes/footer.jsx
+++ practicaReact/src/componentes/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
 		<div className='container'>
 			<div className='row'>
 				<div className='col-12 d-flex flex-column flex-md-row justify-content-between align-items-center'>
-					<img src='./public/logo_provisional-sinfondo.png'/>
+					<img src='/logo_provisional-sinfondo.png'/>
 					<div className='d-flex gap-4 flex-column flex-md-row justify-content-center align-items-center'>
 						<div>
 							<ul className='list-unstyled text-md-end text-center'>
@@ -21,10 +21,10 @@ const Footer = () => {
 						</div>
 						<div>
 							<ul className='list-unstyled fs-2 d-flex flex-md-column flex-sm-row gap-3'>
-								<li><a href='https://wa.me/34653452030?text=Hola!%20Quiero%20información'></a><Whatsapp className="bi bi-whatsapp" /></li>
-								<li><a href=''></a><Instagram className="bi bi-instagram" /></li>
-								<li><a href=''></a><TwitterX className="bi bi-twitter-x" /></li>
-								<li><a href=''></a><Facebook className="bi bi-facebook" /></li>
+								<li><a href='https://wa.me/34653452030?text=Hola!%20Quiero%20información' className='text-dark'><Whatsapp className="bi bi-whatsapp" /></a></li>
+								<li><a href='https://www.instagram.com/adri.navarros?igsh=aWZ5d3hwbmMxajdh' className='text-dark'><Instagram className="bi bi-instagram" /></a></li>
+								<li><a href='https://x.com/Adrnavsav' className='text-dark'><TwitterX className="bi bi-twitter-x" /></a></li>
+								<li><a href='https://www.facebook.com/share/1DAPQS2smq/' className='text-dark'><Facebook className="bi bi-facebook" /></a></li>
 							</ul>
 						</div>
 					</div>
