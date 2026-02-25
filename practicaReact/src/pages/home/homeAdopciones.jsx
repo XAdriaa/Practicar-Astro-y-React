3908c6cf8f5139c6fddffa56039c722111dcc4e3
XAdriaa <s.adria.navarro@gmail.com>
2026-02-25 08:23:53 +0100

    CArrusel de imagenes de la home

--- practicaReact/src/pages/home/homeAdopciones.jsx
+++ practicaReact/src/pages/home/homeAdopciones.jsx
@@ -34,27 +34,63 @@ const HomeAdopciones = () => {
 	}, []);
 
 
-  return (
-	<div className='container'>
-		<div className="row">
-			<div className="col-12">
-				<div className="d-flex justify-content-between text-center flex-md-row flex-column contenedor-cartas-perros">
-					{/**Cartas de los aimales recientemente adoptados */}
-					{adoptados.map((animal) =>(
-						<div key={animal.id_animal} className="card w-50 mx-2 m-sm-1 carta-adoptats">
-							<img src={animal.img} className="card-img-top img-adoptados" alt={animal.nombre}/>
-							<div className="card-body d-flex flex-column">
-								<h3 className="card-title">{animal.nombre}</h3>
-								<p className="card-text">{animal.raza}</p>
-								<p className="card-text">{animal.descripcion}</p>
+	return (
+		<div className='container'>
+			<div className="row">
+				<div className="col-12">
+					{/* Cartas solo visibles en desktop (md y superior) */}
+					<div className="d-none d-md-flex justify-content-between text-center flex-md-row flex-column contenedor-cartas-perros">
+						{adoptados.map((animal) =>(
+							<div key={animal.id_animal} className="card w-50 mx-2 m-sm-1 carta-adoptats">
+								<img src={animal.img} className="card-img-top img-adoptados" alt={animal.nombre}/>
+								<div className="card-body d-flex flex-column">
+									<h3 className="card-title">{animal.nombre}</h3>
+									<p className="card-text">{animal.raza}</p>
+									<p className="card-text">{animal.descripcion}</p>
+								</div>
 							</div>
+						))}
+					</div>
+					{/* Carrusel solo visible en móvil/tablet < 768px */}
+					<div id="carouselExampleCaptions" className="carousel slide d-block d-md-none">
+						<div className="carousel-indicators">
+							{adoptados.map((_, idx) => (
+								<button
+									key={idx}
+									type="button"
+									data-bs-target="#carouselExampleCaptions"
+									data-bs-slide-to={idx}
+									className={idx === 0 ? "active" : ""}
+									aria-current={idx === 0 ? "true" : undefined}
+									aria-label={`Slide ${idx + 1}`}
+								/>
+							))}
 						</div>
-					))}
+						<div className="carousel-inner">
+							{adoptados.map((animal, idx) => (
+								<div className={`carousel-item${idx === 0 ? " active" : ""}`} key={animal.id_animal}>
+									<img src={animal.img} className="d-block w-100" alt="Animal adoptat recient ment" />
+									<div className="carousel-caption d-none d-md-block">
+										<h6>{animal.nombre}</h6>
+									</div>
+								</div>
+							))}
+						</div>
+						<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+							<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span className="visually-hidden">Previous</span>
+						</button>
+						<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+							<span className="carousel-control-next-icon" aria-hidden="true"></span>
+							<span className="visually-hidden">Next</span>
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-  )
+	)
 }
+
+
 
 export default HomeAdopciones
