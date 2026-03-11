be5a36541c5f5e5b8ab601859a7fee6d805c5f35
XAdriaa <s.adria.navarro@gmail.com>
2026-03-11 15:00:42 +0100

    Filtros funcionando falta hacer quie se muestren

--- practicaReact/src/pages/Adopt/AdoptFilter.jsx
+++ practicaReact/src/pages/Adopt/AdoptFilter.jsx
@@ -1,45 +1,105 @@
 import { useEffect, useState } from "react";
-import axios from "axios"
+import { FetchRaces } from "../../server/RequestAPI";
+import { FetchSize } from "../../server/RequestAPI";
+import { FetchFiltreAnimals } from "../../server/RequestAPI";
 
-function gosso() {
-	try {
-		const respuesta = axios.get("https://express-js-on-vercel-azure-zeta-32.vercel.app/api/animales/disponibles");
-		return respuesta.data;
-	} catch(error){
-		console.log("Error al obtener los animales disponibles:" + error);
-		return[];
-	}
-}
 
 const AdoptFilter = () => {
+	const [filtre, setFiltre] = useState({ sex: '', age: '', race: '', size: '' });
+	const [race, setRace] = useState([])
+	const [size, setSize] = useState([])
+	const [animales, setAnimales] = useState([])
+	const ageRange = {
+  		cachorro: { edad_min: 0, edad_max: 1 },
+  		junior:   { edad_min: 1, edad_max: 3 },
+  		adult:    { edad_min: 3, edad_max: 8 },
+  		senior:   { edad_min: 8, edad_max: 99 },
+	}
+
+
+
+	//Handles encargado de manejar el filtro
+	const handleFiltre= (e) => {
+		setFiltre(prev => ({...prev, [e.target.name]: e.target.value}))
+	}
 
 	useEffect(() => {
-		gosso();
-	})
+		const fetchDataRace = async () => {
+			try {
+				const data = await FetchRaces();
+				console.log("Razas de la API",data);
+				setRace(data);
+			} catch(error) {
+				console.log("Error al pasar los datos a los filtros", error);
+			}
+		}
+		fetchDataRace();
+		const fetchDataSize = async () => {
+			try {
+				const data = await FetchSize();
+				console.log("Tamanyo de la API",data);
+				setSize(data);
+			} catch(error) {
+				console.log("Error al pasar los datos a los filtros", error);
+			}
+		}
+		fetchDataSize();
+	}, []);
+
+	useEffect(() => {
+		const fetchAnimal = async () => {
+			const params = {};
+			if(filtre.sex){
+				params.sexo = filtre.sex;
+			}
+			if (filtre.age) {
+  				const range = ageRange[filtre.age]
+  				params.edad_min = range.edad_min
+  				params.edad_max = range.edad_max
+			}
+			if(filtre.race){
+				params.id_raza = filtre.race;
+			}
+			if(filtre.size){
+				params.id_tamano = filtre.size;
+			}
+			const data = await FetchFiltreAnimals(params);
+			setAnimales(data);
+		}
+		fetchAnimal();
+	}, [filtre]);
+
+
+
 
     return (
-		<div className='container-fluid'>
-			<div className='row'>
-				<div className='col-12 d-flex flex-row gap-4 align-items-center py-3 px-4 bg-body-secondary'>
-					<p className='m-0'>Filtres:</p>
-					<select name='tamany' id='tama'>
-						<option value="tamany" disabled defaultValue="tamany" hidden>Tamany</option>
+		<div className="container">
+			<div className="row">
+				<div className="col-12">
+					<select name="sex" value={filtre.sex} onChange={handleFiltre}>
+						<option value = "">Tots</option>
+						<option value = "m">Mascle</option>
+						<option value = "f">Hembra</option>
 					</select>
-					<select name='raça' id='raça'>
-						<option value="raça" disabled defaultValue="raça" hidden>Raça</option>
+					<select name = "race" value={filtre.race} onChange={handleFiltre}>
+						<option value="">Totes</option>
+						{race.map(r => (
+							<option key = {r.id_raza} value = {r.id_raza}>{r.nombre}</option>
+						))}
 					</select>
-					<select name='edat' id='edat'>
-						<option value="edat" disabled defaultValue="edat" hidden>Edat</option>
+					<select name="age" value={filtre.age} onChange={handleFiltre}>
+						<option value="">Totes</option>
+						<option value="cachorro">Cadell</option>
+						<option value="junior">Junior</option>
+						<option value="adult">Adult</option>
+						<option value="senior">Senior</option>
 					</select>
-					<select name='sexe' id='sexe'>
-						<option value="sexe" disabled defaultValue="sexe" hidden>Sexe</option>
+					<select name="size" value={filtre.size} onChange={handleFiltre}>
+						<option value="">Totes</option>
+						{size.map(s => (
+							<option key={s.id_tamano} value={s.id_tamano}>{s.nombre}</option>
+						))}
 					</select>
-				</div>
-			</div>
-			<div className='row'>
-				<div className='col-12'>
-					{}
-
 				</div>
 			</div>
 		</div>
--- practicaReact/src/server/RequestAPI.jsx
+++ practicaReact/src/server/RequestAPI.jsx
@@ -3,7 +3,7 @@ const url = import.meta.env.VITE_API_URL;
 
 
 export const FetchAdoptsHome = async () => {
-	console.log("URL que se está usando:", `${url}animales/adoptados`);
+
 		try {
 			const res = await axios.get(`${url}/animales/adoptados`);
 			return res.data;
@@ -11,3 +11,31 @@ export const FetchAdoptsHome = async () => {
 			console.error("Error en la peticion de animales recientemente adoptados de la Home", error);
 		}
 }
+
+export const FetchRaces = async () => {
+	try{
+		const res = await axios.get(`${url}/razas`);
+		return res.data;
+	} catch (error){
+		console.log("Error a l ahora de obtener las razas de los animales", error);
+	}
+}
+
+export const FetchSize = async () => {
+	try{
+		const res = await axios.get(`${url}/tamanos`);
+		return res.data;
+	} catch (error){
+		console.log("Error a l ahora de obtener el tamanyo de los animales", error);
+	}
+}
+
+export const FetchFiltreAnimals = async (params) => {
+	try{
+		const res = await axios.get(`${url}/animales`, {params});
+		return res.data;
+	} catch(error){
+		console.log("Error al intentar hacer la peticion de la API del filtrado de los animales", error);
+	}
+}
+
