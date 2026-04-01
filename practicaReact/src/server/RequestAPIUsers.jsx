200dea3a98a83f626099433de56f7798c0ffddd9
XAdriaa <s.adria.navarro@gmail.com>
2026-04-01 14:51:07 +0200

    INiciando contexto, al final el token lo usamos en local habra que ver como se hace para añadirle alguna capa de seguridad

--- practicaReact/src/server/RequestAPIUsers.jsx
+++ practicaReact/src/server/RequestAPIUsers.jsx
@@ -1,23 +1,20 @@
 import axios from "axios";
 const url = import.meta.env.VITE_API_URL;
-import Login from "../backoffice/pages/Login/Login";
 
 export const PostDataUsers = async (data) => {
 	try {
 		const res = await axios.post(`${url}/auth/login`, data,{
 		withCredentials:true
 		});
-		console.log(res);
 		console.log("Token recibido de forma correcta", res.data.token);
-		return res.data;
+		const token = res.data.token;
+		localStorage.setItem('token', token)
+		return res.data
 	}
 
 	catch(error){
 		console.error("Error en la peticion de api o en el login", error);
 	}
-}
 
-/**Hay que hasear las contraseñas de la base de datos porque si no no se pueden comprar
- * entonces hay que hacer unnscript para que este lo haga o si no hacerlo en una web online
- * para que asi se puedan comparar.
- */
+
+}
