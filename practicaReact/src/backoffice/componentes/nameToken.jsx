6edeee88e123558d9cbea872c1d8fade3b09e777
Adria <s.adria.navarro@gmail.com>
2026-04-08 18:14:03 +0200

    Crear componente de nametoken

---
+++ practicaReact/src/backoffice/componentes/nameToken.jsx
@@ -0,0 +1,12 @@
+import { jwtDecode } from "jwt-decode";
+import { useAuth } from "../../../context/AuthCOntext";
+
+const nameToken = () => {
+  
+    function nameToken () {
+        
+    }
+
+}
+
+export default nameToken
