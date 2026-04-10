7dff926daef3a0d854496dd915a48f274a500523
Adria <s.adria.navarro@gmail.com>
2026-04-10 13:03:18 +0200

    Empezado cursos

--- practicaReact/src/pages/Train/Train.jsx
+++ practicaReact/src/pages/Train/Train.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Train = () => {
   return (
-	<div className='container'>
+	<div className='container-fluid'>
 		<div className='row'>
-			<div className='col-12 d-flex justify-content-center align-items-center flex-column'>
-				<h1>Cursos disponibles</h1>
-				<div className='card'>
-					<img src='/TrainDogIMG.webp'></img>
-					<div className='card-body'>
-						<p>Curs basic</p>
-					</div>
+			<div className='col-12'>
+				<div className='d-flex justify-content-between'>
+					<a href='#'><button className='btn'>Cursos</button></a>
+					<h1>Cursos disponibles</h1>
+					<NavLink to="/contacte">
+						<button className='btn btn-outline-secondary'>Contacte</button>
+					</NavLink>
 				</div>
+				<img src='/Imagen-adiestramiento.jpg' className='img-fluid' alt="señora entrenando a un perro"/>
 			</div>
 		</div>
 	</div>
