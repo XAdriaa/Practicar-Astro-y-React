4a16edfc58c58d98a3cff4c30faf7acdc44c0dcb
Adria <s.adria.navarro@gmail.com>
2026-04-10 10:24:33 +0200

    si

--- practicaReact/src/route/RoutesAppPrivate.jsx
+++ practicaReact/src/route/RoutesAppPrivate.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import User from '../backoffice/pages/Users/User'
-import Dashboard from '../backoffice/dashboard'
+import Dashboard from '../backoffice/Dashboard'
 
 const RoutesAppPrivate = () => {
   return (
