6fcaffbd8f3cea9219697dbd54ea953a66b97dd5
Adria <s.adria.navarro@gmail.com>
2026-04-10 11:36:40 +0200

    Descripcion incluida

--- practicaReact/src/pages/Adopt/AdoptTarget.jsx
+++ practicaReact/src/pages/Adopt/AdoptTarget.jsx
@@ -12,9 +12,10 @@ const AdoptTarget = ({ data }) => {
 				{data.map((item) => (
 					<div key={item.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center">
 						<div className="card card-size">
+							<img className="card-img-top object-fit-cover" src={item.img} alt="Imagen no disponible" height="200px"></img>
 							<div className="card-body mb-0 pb-0">
-								<img className="card-img-top object-fit-cover" src={item.img} alt="Imagen no disponible" height="200px"></img>
-								<p className="card-title">{item.nombre}</p>
+								<h1 className="card-title">{item.nombre}</h1>
+								<p className="card-body ms-0 ps-0">{item.descripcion}</p>
 							</div>
 							<div className="card-body d-flex mt-0 pt-0 justify-content-end">
 							<button className="btn btn-outline-link text-primary"><FilePlus /></button>
